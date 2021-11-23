4e45b91ddd43995b2ade012730f8adf1c713ddf1
Louisa Noffke <louno@lounovo.com>
2021-11-23 17:48:21 +0000

    added 'dependent: destroy' to association has_many :recipe_ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
 
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
 end
