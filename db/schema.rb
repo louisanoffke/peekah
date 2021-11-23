e8bb95167325c4ee29015cb87106bfe55b46d637
Louisa Noffke <louno@lounovo.com>
2021-11-23 16:07:17 +0000

    added initial ingredient model with name only - to be expanded gradually

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_215310) do
+ActiveRecord::Schema.define(version: 2021_11_23_160410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
