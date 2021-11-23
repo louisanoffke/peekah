607ef5de349afaaf246a1ef6d174f2e31929625b
Louisa Noffke <louno@lounovo.com>
2021-11-23 17:54:48 +0000

    corrected typo in validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,7 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :calorie_content, presence: true, numericality: true
-  validate :scr_url, uniqueness: true
+  validates :scr_url, uniqueness: true
 
   has_many :recipes, through: :recipe_ingredients
 end
