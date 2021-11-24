1456f49ce8580136fb6e70e8518d77c7fca5f3e1
Louisa Noffke <louno@lounovo.com>
2021-11-24 17:49:13 +0000

    form for recipe_ingredients now saving

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,6 +1,8 @@
 class RecipeIngredientsController < ApplicationController
   def index
     @recipe_ingredients = RecipeIngredient.all
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_ingredient = RecipeIngredient.new
   end
 
   def new
@@ -9,13 +11,20 @@ class RecipeIngredientsController < ApplicationController
 
   def create
     @recipe_ingredient = RecipeIngredient.new(recipe_ingredient_params)
+    @recipe_ingredient.recipe = Recipe.find(params[:recipe_id])
+    @recipe_ingredient.ingredient = Ingredient.find(params[:recipe_ingredient][:ingredient_id])
+    @recipe_ingredient.save
 
-    redirect_to recipe_recipe_ingredients_path(@recipe)
+    redirect_to request.referrer
   end
 
   private
 
   def recipe_ingredient_params
-    params.require(:recipe_ingredient).permit(:Ingredient_amount)
+    params.require(:recipe_ingredient).permit(:ingredient_amount)
   end
+
+  # def recipe_ingredient_id
+  #   @recipe_ingredient = RecipeIngredient.find(params[:id])
+  # end
 end
