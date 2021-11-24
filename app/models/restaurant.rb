346f867a8f512fc62c4d3313460d61c99312ebaf
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-24 11:45:10 +0000

    Add base valid. and assoc. for restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,13 @@
 class Restaurant < ApplicationRecord
+  # Validations
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :opentable_link, presence: true
+  validates :deliveroo_link, presence: true
+  validates :justeat_link, presence: true
+
+  # Associations
+  has_many :dishes
+  has_many :users
 end
