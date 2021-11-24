c6ebcff79a64ff357e43f71175ee71d964e862ae
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-24 11:51:44 +0000

    Update validations for links

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,9 +3,6 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :description, presence: true
-  validates :opentable_link, presence: true
-  validates :deliveroo_link, presence: true
-  validates :justeat_link, presence: true
 
   # Associations
   has_many :dishes
