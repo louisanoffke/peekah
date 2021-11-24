e8fe9b03414b0bbe59b23df67c773471ae5291ea
Marie Wright <mariedcpswright@gmail.com>
2021-11-24 12:38:14 +0000

    Basic styling of buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_125953) do
+ActiveRecord::Schema.define(version: 2021_11_23_160410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_11_23_125953) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "method"
