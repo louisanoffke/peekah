8a8fef044e429f7b36671c3bb37d3e886718fa1f
Louisa Noffke <louno@lounovo.com>
2021-11-25 21:05:00 +0000

    debugged seed file - now working

--- db/recipe_ingredient_seeds.rb
+++ db/recipe_ingredient_seeds.rb
@@ -1,14 +1,15 @@
 def build_recipe_ingredients
   recipes = Recipe.all
-  ingredients = Ingredients.all
-  recipes.each.5.times do |recipe|
-    ingredient = ingredients.sample
-    RecipeIngredient.create!(
-      recipe_id: recipe.id,
-      ingredient_id: ingredients.id,
-      ingredient_amount: (1..100).to_a.sample
-    )
+  ingredients = Ingredient.all
+  5.times do
+    recipes.each do |recipe|
+      ingredient = ingredients.sample
+      RecipeIngredient.create!(
+        recipe_id: recipe.id,
+        ingredient_id: ingredient.id,
+        ingredient_amount: (1..100).to_a.sample
+      )
+    end
   end
-
   puts "created #{RecipeIngredient.count} RecipeIngredients."
 end
