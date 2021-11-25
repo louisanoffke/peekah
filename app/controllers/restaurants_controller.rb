0deaab222597148d1b95f5843f87bbdf9c1dd046
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-25 17:07:44 +0000

    Add index action in restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,18 @@ class RestaurantsController < ApplicationController
   before_action :restaurant_id, only: :show
   def show; end
 
+  def index
+    @restaurants = Restaurant.all
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @restaurants.geocoded.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude
+      }
+    end
+  end
+
   private
   def restaurant_id
     @restaurant = Restaurant.find(params[:id])
