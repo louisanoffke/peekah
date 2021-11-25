48502b8eebe1f947fc64fe10c9869a75bb11822b
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-25 17:16:52 +0000

    Fit map to markers

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -15,7 +15,10 @@ export default class extends Controller {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     this._addMarkersToMap();
+    this._fitMapToMarkers();
   }
+
+  // Private methods
 
   _addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -23,5 +26,11 @@ export default class extends Controller {
         .setLngLat([marker.lng, marker.lat])
         .addTo(this.map);
     });
+  }
+
+  _fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
