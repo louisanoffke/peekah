4af86587336b0e83dc074ea2816d861e0bd6fd18
Louisa Noffke <louno@lounovo.com>
2021-11-25 12:56:56 +0000

    removed migration file to rename it

--- db/migrate/20211125124931_add_name_and_restaurant_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndRestaurantToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, name: :string, null: false
-    add_reference :users, :restaurant, foreign_key: true
-  end
-end
