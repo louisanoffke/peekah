21a2548ac7ad9659b9dbb3a43ef28633f58268f5
Louisa Noffke <louno@lounovo.com>
2021-11-25 12:53:54 +0000

    add reference to migration file

--- db/migrate/20211125124931_add_name_and_restaurant_to_user.rb
+++ db/migrate/20211125124931_add_name_and_restaurant_to_user.rb
@@ -1,5 +1,6 @@
 class AddNameAndRestaurantToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, name: :string
+    add_column :users, name: :string, null: false
+    add_reference :users, :restaurant, foreign_key: true
   end
 end
