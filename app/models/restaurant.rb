745873a50397274a92218591e523eb03ec73c7fe
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-25 16:28:49 +0000

    Add conditional save if geocoding found for address

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,7 +6,8 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }
   validates :address, presence: true, length: { minimum: 6 }
   validates :description, presence: true, length: { minimum: 10 }
+  validates :latitude, :longitude, presence: true
   # Geocoding
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  before_validation :geocode, if: :will_save_change_to_address?
 end
