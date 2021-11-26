96cd7e040d799d74fc77ec58c945b76b7cd08d6b
Louisa Noffke <louno@lounovo.com>
2021-11-26 09:37:41 +0000

    removed authentication from dishes index, show, and restaurant show pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,11 @@
 class RestaurantsController < ApplicationController
   before_action :restaurant_id, only: :show
+  skip_before_action :authenticate_user!, only: :show
+
   def show; end
 
   private
+
   def restaurant_id
     @restaurant = Restaurant.find(params[:id])
   end
