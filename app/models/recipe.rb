051a5e59c5418264df72d8710d04c02f4193e544
Louisa Noffke <louno@lounovo.com>
2021-11-27 09:23:34 +0000

    added method to clculate recipe calories to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,12 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :dishes
   has_many :restaurants, through: :dishes
+
+  def calculate_recipe_calories
+    recipe_calories = 0
+    self.recipe_ingredients.each do |ri|
+      recipe_calories += (ri.ingredient_amount * ri.ingredient.calorie_content / 100)
+    end
+    recipe_calories
+  end
 end
