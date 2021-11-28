a83ea1cffe01482a98053ba45bc8d56f6a3f0012
Louisa Noffke <louno@lounovo.com>
2021-11-28 13:37:38 +0000

    add pg search to allergen model

--- app/models/allergen.rb
+++ app/models/allergen.rb
@@ -1,3 +1,13 @@
 class Allergen < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :note ],
+    associated_against: {
+      recipe: :description
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
