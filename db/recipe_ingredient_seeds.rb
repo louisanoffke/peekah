c95956b3a043b54f38c58d2479f6891b90b2e07f
Louisa Noffke <louno@lounovo.com>
2021-11-29 13:00:12 +0000

    added recipe_ingredients for recipe-1

--- db/recipe_ingredient_seeds.rb
+++ db/recipe_ingredient_seeds.rb
@@ -11,5 +11,30 @@ def build_recipe_ingredients
       )
     end
   end
+  # recipe 1 - Gnocchi with Cherry Tomato Garlic and Parmesan
+  RecipeIngredient.create!(
+    recipe_id: 1,
+    ingredient_id: 3,
+    ingredient_amount: 120
+  )
+
+    RecipeIngredient.create!(
+      recipe_id: 1,
+      ingredient_id: 4,
+      ingredient_amount: 150
+    )
+
+    RecipeIngredient.create!(
+      recipe_id: 1,
+      ingredient_id: 5,
+      ingredient_amount: 2
+    )
+
+    RecipeIngredient.create!(
+      recipe_id: 1,
+      ingredient_id: 6,
+      ingredient_amount: 25
+    )
+
   puts "created #{RecipeIngredient.count} RecipeIngredients."
 end
