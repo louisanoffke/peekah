7d9df1a5418934b2d4cc296fb577447d5d6230ff
Louisa Noffke <louno@lounovo.com>
2021-12-01 15:19:50 +0000

    updated order of ingredients to match recipes

--- db/ingredient_seeds.rb
+++ db/ingredient_seeds.rb
@@ -22,85 +22,11 @@ def build_ingredients
   )
 
   Ingredient.create!(
-    name: "Green Chillies",
-    calorie_content: 40,
-    carb_content: 9,
-    protein_content: 1.9
-  )
-
-  Ingredient.create!(
-    name: "Potato Starch",
-    calorie_content: 357,
-    carb_content: 83,
-    protein_content: 7
-  )
-
-  Ingredient.create!(
-    name: "Black Pepper",
-    calorie_content: 318,
-    carb_content: 62,
-    protein_content: 10
-  )
-
-  Ingredient.create!(
-    name: "Lemon Zest",
-    calorie_content: 29,
-    carb_content: 9,
-    protein_content: 1.1
-  )
-
-  Ingredient.create!(
-    name: "Lemon",
-    calorie_content: 29,
-    carb_content: 9,
-    protein_content: 1.1
-  )
-
-  Ingredient.create!(
-    name: "Ground Paprika",
-    calorie_content: 300,
-    carb_content: 60,
-    protein_content: 10
-  )
-
-  Ingredient.create!(
-    name: "Vegetable Oil",
-    calorie_content: 812,
-    protein_content: 0,
-    carb_content: 0,
-    fat_content: 92
-  )
-
-  Ingredient.create!(
-    name: "Parmesan",
-    calorie_content: 431,
-    protein_content: 38,
-    carb_content: 4.1 ,
-    fat_content: 29
-  )
-
-  Ingredient.create!(
-    name: "Squid",
-    calorie_content: 175,
-    protein_content: 18,
-    carb_content: 8,
-    fat_content: 7
-  )
-
-  Ingredient.create!(
-    name: "Baby Squid",
-    calorie_content: 175,
-    protein_content: 18,
-    carb_content: 8,
-    fat_content: 7
-  )
-
-  Ingredient.create!(
-    name: "Maldon Salt",
-    calorie_content: 0,
-    protein_content: 0,
-    carb_content: 0,
-    fat_content:0
+    name: "Gnocchi",
+    calorie_content: 164,
+    protein_content: 3.5,
+    carb_content: 36.1,
+    fat_content: 0.5
   )
 
   Ingredient.create!(
@@ -119,6 +45,13 @@ def build_ingredients
     fat_content: 0.5
   )
 
+  Ingredient.create!(
+    name: "Parmesan",
+    calorie_content: 431,
+    protein_content: 38,
+    carb_content: 4.1,
+    fat_content: 29
+  )
 
   ingredients = %w[
     Pappardelle
@@ -168,6 +101,75 @@ def build_ingredients
       fat_content: Faker::Number.decimal(l_digits: 2, r_digits: 2)
     )
   end
+
+
+  Ingredient.create!(
+    name: "Green Chillies",
+    calorie_content: 40,
+    carb_content: 9,
+    protein_content: 1.9
+  )
+
+  Ingredient.create!(
+    name: "Potato Starch",
+    calorie_content: 357,
+    carb_content: 83,
+    protein_content: 7
+  )
+
+  Ingredient.create!(
+    name: "Black Pepper",
+    calorie_content: 318,
+    carb_content: 62,
+    protein_content: 10
+  )
+
+  Ingredient.create!(
+    name: "Lemon Zest",
+    calorie_content: 29,
+    carb_content: 9,
+    protein_content: 1.1
+  )
+
+  Ingredient.create!(
+    name: "Ground Paprika",
+    calorie_content: 300,
+    carb_content: 60,
+    protein_content: 10
+  )
+
+  Ingredient.create!(
+    name: "Vegetable Oil",
+    calorie_content: 812,
+    protein_content: 0,
+    carb_content: 0,
+    fat_content: 92
+  )
+
+
+  Ingredient.create!(
+    name: "Squid",
+    calorie_content: 175,
+    protein_content: 18,
+    carb_content: 8,
+    fat_content: 7
+  )
+
+  Ingredient.create!(
+    name: "Baby Squid",
+    calorie_content: 175,
+    protein_content: 18,
+    carb_content: 8,
+    fat_content: 7
+  )
+
+  Ingredient.create!(
+    name: "Maldon Salt",
+    calorie_content: 0,
+    protein_content: 0,
+    carb_content: 0,
+    fat_content: 0
+  )
 
   puts "created #{Ingredient.count} ingredients"
 
